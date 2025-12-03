299307a8b7a08f5151d2ef533da3d16d7cf4aaae
romero060523 <andy.romero@tecsup.edu.pe>
2025-12-03 16:38:11 -0500

    Models

--- app/src/main/java/com/tecsup/productmanager/data/model/Product.kt
+++ app/src/main/java/com/tecsup/productmanager/data/model/Product.kt
@@ -1,4 +1,21 @@
 package com.tecsup.productmanager.data.model
 
-class Product {
+data class Product(
+    val id: String = "",
+    val nombre: String = "",
+    val precio: Double = 0.0,
+    val stock: Int = 0,
+    val categoria: String = ""
+) {
+    constructor() : this("", "", 0.0, 0, "")
+
+    fun toMap(): Map<String, Any> {
+        return mapOf(
+            "id" to id,
+            "nombre" to nombre,
+            "precio" to precio,
+            "stock" to stock,
+            "categoria" to categoria
+        )
+    }
 }
