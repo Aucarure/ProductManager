0f9f4916215b07de8d3e01e06645cf2916b1d760
GeraldineSolis <solis.geraldine@tecsup.edu.pe>
2025-12-03 16:42:43 -0500

    Utils

--- app/src/main/java/com/tecsup/productmanager/utils/Constants.kt
+++ app/src/main/java/com/tecsup/productmanager/utils/Constants.kt
@@ -1,4 +1,16 @@
 package com.tecsup.productmanager.utils
 
-class Constants {
+object Constants {
+    const val PRODUCTS_COLLECTION = "products"
+    const val USERS_COLLECTION = "users"
+
+    const val MIN_PASSWORD_LENGTH = 6
+    const val MIN_NOMBRE_LENGTH = 3
+    const val MIN_CATEGORIA_LENGTH = 3
+
+    const val ROUTE_LOGIN = "login"
+    const val ROUTE_REGISTER = "register"
+    const val ROUTE_PRODUCT_LIST = "product_list"
+    const val ROUTE_ADD_PRODUCT = "add_product"
+    const val ROUTE_EDIT_PRODUCT = "edit_product/{productId}"
 }
